6f7169f4103edeba75b0b1c46aa21fed88271269
caqde <caqde@netscape.net>
2023-07-25 15:27:28 -0500

    Add project for connecting to the PostGres Database using EF Core

---
+++ EFCore_DBLibrary/DBLibrary.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_DBLibrary
+{
+    public class DBLibrary: DbContext
+    {
+        private static IConfigurationRoot? _configuration;
+
+        public DBLibrary()
+        {
+
+        }
+
+        public DBLibrary(DbContextOptions options): base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                _configuration = builder.Build();
+                var connectionString = _configuration.GetConnectionString("ShowCaseDB");
+                optionsBuilder.UseNpgsql(connectionString, options => options.EnableRetryOnFailure());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
