ac2a257ed2be66c8bb02639202c5f124d2ae772d
caqde <caqde@netscape.net>
2023-08-11 01:30:50 -0500

    Converted cleardb.sql to PostGreSQL based on the converted cleardbpartial.sql. These SQL statements are not fully tested so changes may need to be made. cleardb.sql in particular has not been executed. So as of now it is not know if it has compile errors.

--- ShowCaseModelUnitTests/cleardb.sql
+++ ShowCaseModelUnitTests/cleardb.sql
@@ -1,52 +1,49 @@
-DECLARE @MyCursor CURSOR;
-DECLARE @tableName varchar(max);
+DO $$
+
+DECLARE MyCursor CURSOR FOR select TABLE_Name from INFORMATION_SCHEMA.TABLES where TABLE_NAME not in ('__MigrationHistory', '__EFMigrationsHistory')
+     tableName text;
 BEGIN
-    SET @MyCursor = CURSOR FOR
-    select TABLE_Name from INFORMATION_SCHEMA.TABLES where TABLE_NAME not in ('__MigrationHistory', '__EFMigrationsHistory');
 
     -- Drop constraints
-    OPEN @MyCursor 
-    FETCH NEXT FROM @MyCursor 
-    INTO @tableName
+    OPEN MyCursor; 
+    FETCH NEXT FROM MyCursor 
+    INTO tableName;
 
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
-      EXEC ('ALTER TABLE ' + @tableName + ' NOCHECK CONSTRAINT ALL');
+    WHILE FOUND LOOP
+      EXECUTE ('ALTER TABLE ' + @tableName + ' NOCHECK CONSTRAINT ALL');
 
       FETCH NEXT FROM @MyCursor 
-      INTO @tableName 
-    END; 
+      INTO tableName; 
+    END LOOP; 
 
-    CLOSE @MyCursor ;
+    CLOSE MyCursor;
 
     -- empty tables
-    OPEN @MyCursor 
-    FETCH NEXT FROM @MyCursor 
-    INTO @tableName
+    OPEN MyCursor; 
+    FETCH NEXT FROM MyCursor 
+    INTO tableName;
 
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
+    WHILE FOUND LOOP
       EXEC ('DELETE from ' + @tableName);
 
-      FETCH NEXT FROM @MyCursor 
-      INTO @tableName 
-    END; 
+      FETCH NEXT FROM MyCursor 
+      INTO tableName; 
+    END LOOP; 
 
-    CLOSE @MyCursor ;
+    CLOSE MyCursor ;
 
     -- Activate constraints
-    OPEN @MyCursor 
+    OPEN MyCursor; 
     FETCH NEXT FROM @MyCursor 
-    INTO @tableName
+    INTO tableName;
 
-    WHILE @@FETCH_STATUS = 0
-    BEGIN
+    WHILE FOUND LOOP
       EXEC ('ALTER TABLE ' + @tableName + ' CHECK CONSTRAINT ALL');
 
-      FETCH NEXT FROM @MyCursor 
-      INTO @tableName 
-    END; 
+      FETCH NEXT FROM MyCursor 
+      INTO tableName; 
+    END LOOP; 
 
-    CLOSE @MyCursor ;
-    DEALLOCATE @MyCursor;
+    CLOSE MyCursor ;
 END;
+$$;
