b84831c23991a87f2e8b01fc9409b828becd7f77
caqde <caqde@netscape.net>
2023-08-13 15:52:57 -0500

    Added GetPrevEntry test and added a function GetCurrentEntryValue as part of refactoring the test code.

--- ShowCaseModelUnitTests/dbObjectModelTests.cs
+++ ShowCaseModelUnitTests/dbObjectModelTests.cs
@@ -36,6 +36,12 @@ namespace ShowCaseModelUnitTests
             }
         }
 
+        private void GetCurrentEntryValue(out string testName, out int testId)
+        {
+            testName = database.DbObjectModel.GetName();
+            testId = database.DbObjectModel.GetiD();
+        }
+
 
         [Fact]
         public void GetIDEmptyDatabase()
@@ -85,34 +91,48 @@ namespace ShowCaseModelUnitTests
         [Fact]
         public void RemoveDatabaseEntry()
         {
+            string testEntryName;
+            int testID;
             database.DbObjectModel.AddEntry("Name1");
-            string testString = database.DbObjectModel.GetName();
-            int testInt = database.DbObjectModel.GetiD();
-            Assert.Equal(1, testInt);
-            Assert.Equal("Name1", testString);
+            GetCurrentEntryValue(out testEntryName, out testID);
+            Assert.Equal(1, testID);
+            Assert.Equal("Name1", testEntryName);
             database.DbObjectModel.DeleteEntry();
-            testString = database.DbObjectModel.GetName();
-            testInt = database.DbObjectModel.GetiD();
-            Assert.Equal(1, testInt);
-            Assert.Equal("NULL", testString);
+            GetCurrentEntryValue(out testEntryName, out testID);
+            Assert.Equal(1, testID);
+            Assert.Equal("NULL", testEntryName);
         }
 
         [Fact]
         public void GetNextEntry()
         {
+            string testEntryName;
+            int testID;
             AddIndividualEntries(3);
             database.DbObjectModel.GetFirstEntry();
-            string testString = database.DbObjectModel.GetName();
-            int testInt = database.DbObjectModel.GetiD();
-            Assert.Equal(1, testInt);
-            Assert.Equal("Name0", testString);
+            GetCurrentEntryValue(out testEntryName, out testID);
+            Assert.Equal(1, testID);
+            Assert.Equal("Name0", testEntryName);
             database.DbObjectModel.NextEntry();
-            testString = database.DbObjectModel.GetName();
-            testInt = database.DbObjectModel.GetiD();
-            Assert.Equal(2, testInt);
-            Assert.Equal("Name1", testString);
+            GetCurrentEntryValue(out testEntryName, out testID);
+            Assert.Equal(2, testID);
+            Assert.Equal("Name1", testEntryName);
         }
 
+        [Fact]
+        public void GetPreviousEntry()
+        {
+            string testEntryName;
+            int testID;
+            AddIndividualEntries(4);
+            GetCurrentEntryValue(out testEntryName, out testID);
+            Assert.Equal(4, testID);
+            Assert.Equal("Name3", testEntryName);
+            database.DbObjectModel.PrevEntry();
+            GetCurrentEntryValue(out testEntryName, out testID);
+            Assert.Equal(3, testID);
+            Assert.Equal("Name2", testEntryName);
+        }
 
 
     }
