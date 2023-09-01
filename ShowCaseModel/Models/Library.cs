8d46bd8ae1d894a671fd8cd05497e1c8ad5e2f63
caqde <caqde@netscape.net>
2023-09-01 00:32:12 -0500

    Added more tests and supporting code for adding and retrieving values from the Library database tables specifically for adding and retrieving books and patrons.

--- ShowCaseModel/Models/Library.cs
+++ ShowCaseModel/Models/Library.cs
@@ -13,7 +13,7 @@ namespace ShowCaseModel.Models
     public class Library : ILibrary
     {
         private readonly DBFactory dBFactory;
-        
+
         public Library()
         {
             dBFactory = new DBFactory();
@@ -31,6 +31,15 @@ namespace ShowCaseModel.Models
             database.Authors.Add(author);
             database.SaveChanges();
             libraryAuthor.Id = author.Id;
+        }
+
+        public void AddPatron(LibraryPatron libraryPatron)
+        {
+            var database = dBFactory.GetDbContext();
+            Patron patron = new Patron { Name = libraryPatron.Name, City = libraryPatron.City, PhoneNumber = libraryPatron.PhoneNumber, StreetAddress = libraryPatron.StreetAddress, PostalCode = libraryPatron.PostalCode };
+            database.Patrons.Add(patron);
+            database.SaveChanges();
+            libraryPatron.Id = patron.Id;
         }
 
         public void AddPublisher(LibraryPublisher libraryPublisher)
@@ -67,6 +76,55 @@ namespace ShowCaseModel.Models
             else
             {
                 return new LibraryPublisher { Description = publisher.Description, Id = publisher.Id, Name = publisher.Name };
+            }
+        }
+
+        public void AddBook(LibraryBook book, int AuthorID, int PublisherID)
+        {
+            var database = dBFactory.GetDbContext();
+            var author = database.Authors.FirstOrDefault(x => x.Id ==  AuthorID);
+            var publisher = database.Publishers.FirstOrDefault(x => x.Id == PublisherID);
+            if (publisher == null || author == null)
+            {
+                //throw exception or error?
+                return;
+            }
+            else
+            {
+                Book newBook = new Book() { Author = author, Publisher = publisher, Description = book.Description, ISBN = book.ISBN, Title = book.Title };
+                database.Books.Add(newBook);
+                database.SaveChanges();
+                book.Id = newBook.Id;
+            }
+        }
+
+        public LibraryBook GetBook(int Id)
+        {
+            var db = dBFactory.GetDbContext();  
+            var book = db.Books.FirstOrDefault(x => x.Id == Id);
+            if (book == null)
+            {
+                return null;
+            }
+            else
+            {
+                LibraryBook newBook = new LibraryBook() { ISBN = book.ISBN, Description = book.Description, Title = book.Title, Id = book.Id };
+                return newBook;
+            }
+        }
+
+        public LibraryPatron GetPatron(int Id)
+        {
+            var db = dBFactory.GetDbContext();
+            var patron = db.Patrons.FirstOrDefault(x => x.Id == Id);
+            if (patron == null)
+            {
+                return null;
+            }
+            else
+            {
+                LibraryPatron newPatron = new LibraryPatron() { PhoneNumber = patron.PhoneNumber, Id = patron.Id, City = patron.City, Name = patron.Name, PostalCode = patron.PostalCode, StreetAddress = patron.StreetAddress };
+                return newPatron;
             }
         }
     }
--- ShowCaseModelUnitTests/LibraryTests.cs
+++ ShowCaseModelUnitTests/LibraryTests.cs
@@ -19,18 +19,32 @@ namespace ShowCaseModelUnitTests
             DatabaseTracker.New(false);
         }
 
-        private int AddAuthorToDatabase(string name, string Biography)
+        private LibraryAuthor AddAuthorToDatabase(string name, string Biography)
         {
             LibraryAuthor author = new LibraryAuthor() { Biography = Biography, Name = name };
             database.Library.AddAuthor(author);
-            return author.Id;
+            return author;
         }
 
-        private int AddPublisherToDatabase(string name, string Description)
+        private LibraryPublisher AddPublisherToDatabase(string name, string Description)
         {
             LibraryPublisher publisher = new LibraryPublisher() { Description = Description, Name = name };
             database.Library.AddPublisher(publisher);
-            return publisher.Id;
+            return publisher;
+        }
+
+        private LibraryBook AddBookToDatabase(string name, string description, int ISBN, LibraryAuthor author, LibraryPublisher publisher)
+        {
+            LibraryBook book = new LibraryBook() { Title = name, Description = description, ISBN = ISBN };
+            database.Library.AddBook(book, author.Id, publisher.Id);
+            return book;
+        }
+
+        private LibraryPatron AddPatronToDatabase(string name, string address, int postal, string city, string number)
+        {
+            LibraryPatron patron = new LibraryPatron() { Name = name, City = city, PhoneNumber = number, PostalCode = postal, StreetAddress = address };
+            database.Library.AddPatron(patron);
+            return patron;
         }
 
         [Fact]
@@ -93,6 +107,86 @@ namespace ShowCaseModelUnitTests
             Assert.Equal("TestDescription", TestData.Description);
             TestData = database.Library.GetPublisher(6);
             Assert.Null(TestData);
+        }
+
+        [Fact]
+        public void AddBook()
+        {
+            AddAuthorToDatabase("Test", "TestBiography");
+            AddPublisherToDatabase("Test", "TestDescription");
+            var TestPublisher = database.Library.GetPublisher(1);
+            var TestAuthor = database.Library.GetAuthor(1);
+            Assert.NotNull(TestPublisher);
+            Assert.NotNull(TestAuthor);
+            LibraryBook book = new LibraryBook() { Description = "TestDescription", ISBN = 1, Title = "Test" };
+            database.Library.AddBook(book, TestAuthor.Id, TestPublisher.Id);
+            var TestBook = database.Library.GetBook(book.Id);
+            Assert.NotNull(TestBook);
+            Assert.Equal(book.Description, TestBook.Description);
+            Assert.Equal(book.Title, TestBook.Title);
+            Assert.Equal(book.ISBN, TestBook.ISBN);
+        }
+
+        [Fact]
+        public void GetBook()
+        {
+            var author = AddAuthorToDatabase("Test", "TestBiography");
+            var publisher = AddPublisherToDatabase("Test", "TestDescription");
+            var book1 = AddBookToDatabase("Test", "TestDescription", 1, author, publisher);
+            var book2 = AddBookToDatabase("Test2", "TestDescription2", 12, author, publisher);
+            var book3 = AddBookToDatabase("Test3", "TestDescription3", 1234, author, publisher);
+            var testBook1 = database.Library.GetBook(book1.Id);
+            Assert.NotNull (testBook1);
+            Assert.Equal (book1.Description, testBook1.Description);
+            Assert.Equal (book1.Title, testBook1.Title);
+            Assert.Equal (book1.ISBN, testBook1.ISBN);
+            var testBook2 = database.Library.GetBook (book3.Id);
+            Assert.NotNull (testBook2);
+            Assert.Equal (book3.Description, testBook2.Description);
+            Assert.Equal (book3.Title, testBook2.Title);
+            Assert.Equal (book3.ISBN, testBook2.ISBN);
+            var testBook3 = database.Library.GetBook(6);
+            Assert.Null (testBook3);
+        }
+
+        [Fact]
+        public void AddPatron()
+        {
+            LibraryPatron patron = new LibraryPatron();
+            patron.StreetAddress = "TestAddress";
+            patron.PhoneNumber = "12345";
+            patron.City = "TestCity";
+            patron.Name = "Test";
+            patron.PostalCode = 12345;
+            database.Library.AddPatron(patron);
+            var testData = database.Library.GetPatron(patron.Id);
+            Assert.NotNull (testData);
+            Assert.Equal(patron.Id, testData.Id);
+            Assert.Equal(patron.Name, testData.Name);
+            Assert.Equal(patron.City, testData.City);
+            Assert.True (testData.PhoneNumber == testData.PhoneNumber);
+            Assert.Equal(patron.PostalCode, testData.PostalCode);
+            Assert.Equal(patron.StreetAddress, testData.StreetAddress);
+        }
+
+        [Fact]
+        public void GetPatron()
+        {
+            var patron1 = AddPatronToDatabase("Test", "TestAddress", 12345, "TestCity", "12345");
+            var patron2 = AddPatronToDatabase("Test2", "TestAddress2", 23456, "TestCity2", "23456");
+            var patron3 = AddPatronToDatabase("Test3", "TestAddress3", 34567, "TestCity3", "34567");
+            var TestPatron1 = database.Library.GetPatron(patron1.Id);
+            Assert.NotNull(TestPatron1);
+            Assert.Equal(patron1.Name, TestPatron1.Name);
+            Assert.Equal(patron1.City, TestPatron1.City);
+            Assert.Equal(patron1.PostalCode, TestPatron1.PostalCode);
+            var TestPatron2 = database.Library.GetPatron(patron3.Id);
+            Assert.NotNull(TestPatron2);
+            Assert.Equal(patron3.PhoneNumber, TestPatron2.PhoneNumber);
+            Assert.Equal(patron3.Name, TestPatron2.Name);
+            Assert.Equal(patron3.StreetAddress, TestPatron2.StreetAddress);
+            var TestPatron3 = database.Library.GetPatron(6);
+            Assert.Null(TestPatron3);
         }
     }
 }
