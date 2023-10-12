5048287246c091e8db38bf0e7ceec87f20b06dbb
caqde <caqde@netscape.net>
2023-10-12 15:04:52 -0500

    WPFView -Setup design of interface and bindings for Library pages.

--- WinFormDatabase/LibraryDatabase/LibraryAuthor.Designer.cs
+++ WinFormDatabase/LibraryDatabase/LibraryAuthor.Designer.cs
@@ -94,7 +94,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(86, 9);
+            label1.Location = new Point(104, 9);
             label1.Name = "label1";
             label1.Size = new Size(49, 15);
             label1.TabIndex = 2;
